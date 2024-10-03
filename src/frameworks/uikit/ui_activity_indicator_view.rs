e611f5eba88090dee023ee8d8946cfaa1969be2c
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-10-03 19:23:24 +0200

    Update ui_activity_indicator_view.rs

--- src/frameworks/uikit/ui_activity_indicator_view.rs
+++ src/frameworks/uikit/ui_activity_indicator_view.rs
@@ -5,8 +5,8 @@
  */
 //! `UIActivityIndicatorView`.
 
-use crate::frameworks::foundation::NSInteger;
-use crate::objc::{id, msg, ClassExports};
+use crate::frameworks::foundation::{NSInteger, NSUInteger};
+use crate::objc::{id, msg, nil, ClassExports};
 use crate::objc_classes;
 
 type UIActivityIndicatorViewStyle = NSInteger;
@@ -22,12 +22,54 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; this init]
 }
 
+- (())setHidesWhenStopped:(bool)hides {
+    log!("TODO: setHidesWhenStopped:{}", hides);
+}
+
+- (())setActivityIndicatorViewStyle :(bool)activity {
+    log!("TODO: setActivityIndicatorViewStyle:{}", activity);
+}
+
+- (())sizeThatFits:(bool)fits {
+    log!("TODO: sizeThatFits:{}", fits);
+}
+- (id)sizeToFit {
+    nil
+}
+
 - (())startAnimating {
     log!("TODO: [(UIActivityIndicatorView*){:?} startAnimating", this);
 }
 
 - (())stopAnimating {
     log!("TODO: [(UIActivityIndicatorView*){:?} stopAnimating", this);
+}
+
+@end
+
+@implementation UIActionSheet: NSObject
+- (id)addButtonWithTitle:(NSUInteger)_title {
+    msg![env; this init]
+}
+
+- (id)showInView:(NSUInteger)_view {
+    msg![env; this init]
+}
+
+- (())setActionSheetStyle:(bool)action {
+    log!("TODO: setActionSheetStyle:{}", action);
+}
+
+- (())setTitle:(bool)title {
+    log!("TODO: setTitle:{}", title);
+}
+
+- (())setDelegate:(bool)delegate {
+    log!("TODO: setDelegate:{}", delegate);
+}
+
+- (())initWithTitle:(NSInteger)title delegate:(bool)_delegate cancelButtonTitle:(bool)_button destructiveButtonTitle:(bool)_destructive otherButtonTitles:(bool)_titles {
+    // TODO
 }
 
 @end
